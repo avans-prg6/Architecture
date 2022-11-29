dea8ae6c19e88180c7fbbfd6b84ebc4e55bc4c23
Ger <gbj.saris@avans.nl>
2022-11-29 22:50:24 +0100

    cleanup

--- Architecture.Uitwerking/Architecture.ASP/Startup.cs
+++ Architecture.Uitwerking/Architecture.ASP/Startup.cs
@@ -24,21 +24,24 @@ namespace Architecture.ASP
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime.
+        // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<MyContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews()
-                .AddRazorRuntimeCompilation(); //Toegevoegd via het nuger package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation 
+                .AddRazorRuntimeCompilation(); //Toegevoegd via het NuGet package
+                                               //Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation 
 
             services.AddScoped<IBeschikbaarheidService, BeschikbaarheidService>();
             services.AddScoped<IVakantieService, VakantiedagenService>();
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime.
+        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MyContext context) 
         {
             //toegevoegd voor demo week 5
